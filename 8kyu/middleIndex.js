a8b0d6a279a967a575bb87b159110bb0d945d4b1
seekingcat <llana_a154@yahoo.co.uk>
2024-07-13 10:21:51 +0100

    kata finding the middle index of a string

---
+++ 8kyu/middleIndex.js
@@ -0,0 +1,20 @@
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+function getMiddle(s) {
+  let strLength = s.length;
+  let middleIndex = Math.floor(strLength / 2);
+  if (strLength === 1) {
+    return s;
+  } else if (strLength % 2 !== 0) {
+    return s[middleIndex];
+  } else {
+    return s.substring(middleIndex - 1, middleIndex + 1);
+  }
+}
+
+//  OTHER SOLUTIONS
+
+// function getMiddle(s)
+// {
+//   return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+// }
